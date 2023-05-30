b9986e250628f190a4a4e9d34c58e03359c0d056
Jay Prajapati <92544545+jayprajapati9@users.noreply.github.com>
2023-05-30 16:02:25 +0530

    Add files via upload

---
+++ secondprogram.java
@@ -0,0 +1,17 @@
+package javatraining;
+
+import java.util.Scanner;
+
+public class secondprogram {
+
+	    public static void main(String[] args) 
+	    {
+	        int a, b,c;
+	        Scanner s = new Scanner(System.in);
+	        System.out.println("Enter two numbers:");
+	        a = s.nextInt();
+	        b = s.nextInt();
+	        c = (a>b)?a:b;
+	        System.out.println("Largest Number:"+c);
+	    }
+}
