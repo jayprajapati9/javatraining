c132948ae8bfeefd56d2823bb6101e83e8f43516
Jay Prajapati <92544545+jayprajapati9@users.noreply.github.com>
2023-05-31 19:42:24 +0530

    Update statickeyword.java

--- statickeyword/statickeyword.java
+++ statickeyword/statickeyword.java
@@ -23,39 +23,3 @@ public class statickeyword {
         System.out.println("Total books in the bookstore: " + totalBooksInStore);
     }
 }
-
-
-/*
- * 
- * package javatraining;
- * 
- * 
- * public class statickeyword { // static variable static int staticValue = 10;
- * 
- * // static method static void staticMethod() {
- * System.out.println("Hello from staticMethod()");
- * System.out.println("Static variable value: " + staticValue); }
- * 
- * // non-static variable int nonStaticValue = 20;
- * 
- * // non-static method void nonStaticMethod() {
- * System.out.println("Hello from nonStaticMethod()");
- * System.out.println("Non-static variable value: " + nonStaticValue); }
- * 
- * public static void main(String[] args) { // Accessing static variable without
- * creating an object
- * System.out.println("Static variable accessed without object: " +
- * statickeyword.staticValue);
- * 
- * // Accessing static method without creating an object
- * statickeyword.staticMethod();
- * 
- * // Creating an object of the class statickeyword obj = new statickeyword();
- * 
- * // Accessing non-static variable using object
- * System.out.println("Non-static variable accessed using object: " +
- * obj.nonStaticValue);
- * 
- * // Accessing non-static method using object obj.nonStaticMethod(); } }
- * 
- */
